ce175d7fd044c9d98ba8640611fe2d4fae479626
oakwoodgates <wpguru4u@gmail.com>
2023-02-16 22:27:47 -0500

    wallet connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,43 @@
-import '../styles/globals.css'
+import React, { useMemo } from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter
+} from "@solana/wallet-adapter-wallets";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { clusterApiUrl } from "@solana/web3.js";
 
-export default MyApp
+require("@solana/wallet-adapter-react-ui/styles.css");
+require("../styles/globals.css");
+require ("../styles/Home.module.css");
+
+const App = ({ Component, pageProps }) => {
+  // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can provide a custom RPC endpoint here
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  //const endpoint = clusterApiUrl('devnet')
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded
+  const wallets = [
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter()
+    ]
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default App;
